2b2bb589162857f70611b99a5e9f39c280bfd3a5
Amine <amine@mouafik.fr>
2020-09-19 16:46:03 +0100

    Fixes undefined Franz as suggested by @vantezzen

--- uncompressed/gotomeeting/webview.js
+++ uncompressed/gotomeeting/webview.js
@@ -3,7 +3,7 @@
 const path = require('path');
 
 module.exports = Ferdi => {
-  Franz.injectCSS(path.join(__dirname, 'service.css'));
+  Ferdi.injectCSS(path.join(__dirname, 'service.css'));
   if(document.location.href.match("after")){
     	document.location.href="https://app.gotomeeting.com/home.html";
     }
